c699a2116a61b2111650f8c1c548c4270e0584f1
alwaysintreble <mmmcheese158@gmail.com>
2024-07-21 16:24:56 -0500

    oop

--- GenerateMystery.py
+++ GenerateMystery.py
@@ -112,7 +112,7 @@ def main():
         for game in games:
             game_options = GameSettings(mystery["name"], mystery["description"], mystery["requires"], game, mystery[game])
             yaml.dump(dict(game_options), file)
-            if not last_key:
+            if game != last_key:
                 file.write("\n---\n\n")
     print(Fore.GREEN + "\nOutputted settings file to `./output/weights.yaml`")
     meta_path = os.path.join("output", "meta.yaml")
