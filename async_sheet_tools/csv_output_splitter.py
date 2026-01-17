ca337778d382faf6ee51c4b150dcdb6e780caf2f
StripesOO7 <54711792+StripesOO7@users.noreply.github.com>
2026-01-17 15:50:39 +0100

    added default names as well as a file selection dialogue for the input csv-file. also added parameters in the python script is executed from CMD for input and output file.

--- async_sheet_tools/csv_output_splitter.py
+++ async_sheet_tools/csv_output_splitter.py
@@ -1,21 +1,25 @@
 import yaml
 import csv
+import argparse
+import tkinter as tk
+from tkinter import filedialog
 from pyexcel_ods import save_data
 from collections import OrderedDict
 
-def format_output_csv():
+def format_output_csv(filename:str = "output.csv", output_filename:str = "formatted_options.ods"):
     # this simply reads the gameoption_whitelist.yaml into a dictionary to have all game relevant options available
     # when filtering for the game name
     with open("gameoption_whitelist.yaml", "r") as options:
         options_dict = yaml.load(options, Loader=yaml.FullLoader)
-        print(options_dict)
+        # print(options_dict)
 
     # this creates the template for the output where all the actually rolled options gets appended line by line for
     # each slot of a specific game
     generated_options_per_slot = {game:[options_dict[game]] for game in options_dict.keys()}
 
+
     # reading the generated csv
-    with open("output.csv", "r") as csv_stream:
+    with open(filename, "r") as csv_stream:
         options_csv = csv.reader(csv_stream)
         # looping over each line from the csv
         for index, row in enumerate(options_csv):
@@ -33,7 +37,7 @@ def format_output_csv():
                     slot_options = ["",""]
                     # actually filter the whitelisted options based on the index of a given option into the temporary
                     # list
-                    # skip the first 3 options because those are only there to make is easy to past into the big
+                    # skip the first 2 options because those are only there to make is easy to past into the big
                     # async sheet
                     for option in game_option_whitelist[2:]:
                         option_index = all_options_indices[option]
@@ -51,8 +55,40 @@ def format_output_csv():
         # maybe it's possible to do that all in one sheet but this is a simple and clean way for now
         output.update({game: [*generated_options_per_slot[game]]})
     # actually save as .ods
-    save_data("formatted_options.ods", output)
+    save_data(f"{output_filename}.ods", output)
     print("done")
 
 if __name__ == "__main__":
-    format_output_csv()
+    cmd_parser = argparse.ArgumentParser(
+        prog="Big Async Options Sheet creator",
+        description="""This script is used for converting the generations options-CSV into the format of the 
+        Big-Async spreadsheet and separating each games the options  
+        """,
+    )
+    cmd_parser.add_argument("-I", "--input", type=str)
+    cmd_parser.add_argument("-O", "--output", type=str)
+    cmd_args = cmd_parser.parse_args()
+
+    if cmd_args.input is None: # check for cmd parameter used for input filename/filepath
+        root = tk.Tk()
+        root.withdraw()
+        print("""
+Please select the CSV-File you got from the generation output.
+        """)
+        input_file_path = tk.filedialog.askopenfile()
+        input_file_path = input_file_path.name
+    else:
+        input_file_path = cmd_args.input
+
+    if cmd_args.output is None: # check for cmd parameter used for output filename
+        output_file_name = "formatted_options"
+    else:
+        output_file_name = cmd_args.output
+    print(f"""
+Using '{input_file_path}' as input filename.
+Using '{output_file_name}' as output filename.
+    """)
+    # print(f"input csv-file: {input_file_path}")
+    # print(f"outout ods-file: {output_file_path}")
+
+    format_output_csv(input_file_path, output_file_name)
